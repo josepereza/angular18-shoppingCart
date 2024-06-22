f33a59b3d510f210c4b02de76e71be777d07ee3e
josepereza <josepereza66@gmail.com>
2024-06-22 21:42:25 +0200

    first commit9

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,25 +1,56 @@
-import { Component, Injector, OnInit, Signal, inject, input } from '@angular/core';
+import {
+  Component,
+  Injector,
+  OnInit,
+  Signal,
+  inject,
+  input,
+} from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../interfaces/product';
 import { CurrencyPipe } from '@angular/common';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [CurrencyPipe],
   templateUrl: './details.component.html',
-  styleUrl: './details.component.css'
+  styleUrl: './details.component.css',
 })
 export default class DetailsComponent implements OnInit {
   //@Input() id!: number;
-  id  = input.required<number>();    // Signal<number>
-  
-  productoService=inject(ProductService)
-  producto!:Signal<Product | undefined>
-  injector=inject(Injector)
+  id = input.required<number>(); // Signal<number>
 
+  productoService = inject(ProductService);
+  producto!: Signal<any>;
+  injector = inject(Injector);
+  cartService= inject(CartService)
+  cantidad:number=1
   ngOnInit(): void {
-  this.producto=toSignal(this.productoService.getProductById(this.id()),{injector:this.injector});
+    this.producto = toSignal(this.productoService.getProductById(this.id()), {
+      injector: this.injector,
+    });
+  }
+
+  inc() {
+
+    if (this.producto()!.qty != 1) {
+      this.cantidad++
+    }else {
+      this.cantidad=1
+    }
+    
+  }
+  dec() {
+    if (this.producto()!.qty != 1) {
+     this.cantidad--
+    }
+  }
+  add(){
+
+    console.log(this.cantidad)
+    this.cartService.addProductToCart(this.producto()!, this.cantidad)
   }
 }
